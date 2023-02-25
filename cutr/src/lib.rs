8d3bf51f6592644155c182a7eafdcfe07b8106ab
thirtiseven <ntlihy@gmail.com>
2023-02-25 23:38:59 +0800

    [WIP] cut in rust

---
+++ cutr/src/lib.rs
@@ -0,0 +1,20 @@
+use crate::Extract::*;
+use clap::{App, Arg};
+use std::{error::Error, ops::Range};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+type PositionList = Vec<Range<usize>>;
+
+#[derive(Debug)]
+pub enum Extract {
+    Fields(PositionList),
+    Bytes(PositionList),
+    Chars(PositionList),
+}
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    delimiter: u8,
+    extract: Extract,
+}
