872b14c64ef90712aa760cc5065148b4551808b3
thirtiseven <ntlihy@gmail.com>
2023-03-12 15:15:52 +0800

    add tailr

--- commr/src/lib.rs
+++ commr/src/lib.rs
@@ -30,7 +30,7 @@ pub struct Config {
 pub fn get_args() -> MyResult<Config> {
     let matches = App::new("commr")
         .version("0.1.0")
-        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .author("Haoyang Li <thirtiseven@hotmail.com>")
         .about("Rust comm")
         .arg(
             Arg::with_name("file1")
